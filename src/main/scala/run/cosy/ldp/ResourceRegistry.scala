4556349e89a26801a2ef35c0f79a0c2831ae6acb
Henry Story <henry.story@bblfish.net>
2023-03-24 12:19:17 +0100

    DirTree (was ATree) is now stack safe

--- src/main/scala/run/cosy/ldp/ResourceRegistry.scala
+++ src/main/scala/run/cosy/ldp/ResourceRegistry.scala
@@ -9,10 +9,9 @@ package run.cosy.ldp
 import akka.actor.typed.*
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import cats.data.NonEmptyList
 import run.cosy.ldp.fs.BasicContainer
-import scalaz.NonEmptyList
 
-import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicReference
 import scala.annotation.tailrec
 import scala.collection.immutable.HashMap
@@ -28,14 +27,14 @@ trait PathDB[T]:
    /** todo: this should be a tree of paths so that if a branch higher up dies todo: then the whole
      * subtree can be pruned
      */
-   val pathMap: AtomicReference[Option[ATree[T]]] = new AtomicReference(None)
+   val pathMap: AtomicReference[Option[DirTree[T]]] = new AtomicReference(None)
 
    /** add an actor Ref to the local DB */
    def addActorRef(path: Uri.Path, actorRef: T): Unit =
      pathMap.updateAndGet { tree =>
         val pathLst = pathToList(path)
         tree match
-         case None      => if (pathLst.isEmpty) then Some(ATree(actorRef)) else None
+         case None      => if (pathLst.isEmpty) then Some(DirTree(actorRef)) else None
          case Some(ref) => Some(ref.insert(actorRef, pathLst))
      }
 
@@ -74,80 +73,153 @@ object ResourceRegistry extends ExtensionId[ResourceRegistry]:
    def createExtension(system: ActorSystem[?]): ResourceRegistry =
      new ResourceRegistry(system)
 
-/** Immutable tree structure to keep path to references could perhaps use cats.free.Cofree. A Tree
-  * is very likely a comonad. (which would make coMonad functions available - would they be useful?)
+/** Immutable Rose tree structure to keep path to references could perhaps use
+  * {{{
+  * type DirTree[A] = cats.free.Cofree[HashMap[String,_],A]
+  * }}}
+  * see:
+  * [[https://app.gitter.im/#/room/#typelevel_cats:gitter.im/$jOKyOT_G008b4VZxjdqvfojzyK-q4xaLykQMsg6HJ20 Feb 2021 discussion on gitter]].
+  * Especially this talk by @tpolecat
+  * [[http://tpolecat.github.io/presentations/cofree/slides#1 Fun & Games with Fix, Cofree, and Doobie!]].
+  * It is not clear though exactly what advantage the Cofree library gives one though for this
+  * project. Does the library give one ways of making changes into the structure?
+  * ([[https://www.youtube.com/watch?v=7xSfLPD6tiQ video of the talk]])
+  *
+  * To make changes the data structure one could use Lenses. I actually have a very close example
+  * where I was modelling a web server and making changes to it using Monocle.
+  * [[https://github.com/bblfish/lens-play/blob/master/src/main/scala/server/Server.scala lens-play Server.scala]]
+  * Here we only need to model the Containers so it simplifies a lot.
+  *
+  * Otherwise in order to make the transformations tail recursive one has to use a Zipper to build a
+  * Path to the changed node and then reconstruct the tree with that as explained in this
+  * [[https://stackoverflow.com/questions/17511433/scala-tree-insert-tail-recursion-with-complex-structure Stack Overflow Answer]]
+  *
+  * Would one reason to use Cofree be that one could use double tail recursion, and that could be
+  * more efficient than the Path deconstruction and data reconstruction route?
+  * [[https://stackoverflow.com/questions/55042834/how-to-make-tree-mapping-tail-recursive stack overflow answer]]
+  * One of those posts pointed out that using tail recursion slows things down by a factor of 4, and
+  * speed should be top priority here.
+  *
+  * So really we should either use a lens lib or implement it ourselves. Here we choose to reduce
+  * dependencies.
   */
-case class ATree[A](a: A, kids: HashMap[String, ATree[A]] = HashMap()):
-   type Path = List[String]
+object DirTree:
+  // a path of directory names, starting from the root going inwards
+  // todo: could one also use directly a construct from Akka actor path?
+  type Path = List[String]
+
+  /** A Path of DirTree[A]s is used to take apart a DirTree[A] structure, in the reverse direction.
+    * Note the idea is that each link (name, dt) points from dt via name in the hashMap to the next
+    * deeper node. The APath is in reverse direction so we have List( "newName" <- dir2 , "dir2in1"
+    * <- dir1 , "dir1" <- rootDir ) An empty List would just refer to the root DirTree[A]
+    */
+  type ALink[A] = (String, DirTree[A])
+  type APath[A] = List[ALink[A]]
+  // the first projection is the remaining path
+  type SearchAPath[A] = (Path, APath[A])
+
+  extension [A](dt: DirTree[A])
+     /** @param at path to resource A
+       * @return
+       * a pair of the remaining path and A
+       */
+     @tailrec
+     def findClosest(at: Path): (Path, A) =
+       at match
+         case Nil => (at, dt.a)
+         case name :: tail =>
+           dt.kids.get(name) match
+             case None => (at, dt.a)
+             case Some(tree) =>
+               tree.findClosest(tail) //this should be recursive!
+
+     /** note we can only find the closest path to something if the path is not empty */
+     def toClosestAPath(path: Path): SearchAPath[A] =
+       @tailrec
+       def loop(dt: DirTree[A], path: Path, result: APath[A]): SearchAPath[A] =
+         path match
+           case Nil => (Nil, result)
+           case name :: rest =>
+             if dt.kids.isEmpty then (rest, (name, dt) :: result)
+             else
+               dt.kids.get(name) match
+                 case None => (rest, (name, dt) :: result)
+                 case Some(dtchild) =>
+                   loop(dtchild, rest, (name, dt) :: result)
+       end loop
+       loop(dt, path, Nil)
+     end toClosestAPath
+
+  
+//     @tailrec
+//     def deleteNE(name: String, remaining: List[String]): DirTree[A] =
+//       dt.kids.get(name) match
+//         case None => dt
+//         case Some(tree) =>
+//           remaining match
+//             case Nil => DirTree(dt.a, dt.kids - name)
+//             case head :: tail =>
+//               val alt = tree.deleteNE(head, tail) //
+//               DirTree(dt.a, dt.kids + (name -> alt))
+//
+
+end DirTree
+
+/** Another option could have been
+  * {{{
+  * import cats.free.Cofree
+  *
+  * type HMap[X] = HashMap[String,X]
+  * type DirTree[A] = Cofree[HMap[_],A]
+  * }}}
+  */
+case class DirTree[A](a: A, kids: HashMap[String, DirTree[A]] = HashMap()):
+   import DirTree.*
 
    /** Insert a new A at given path, and ignore any existing subtrees at that position. This makes
      * sense for our main use case of ActorRef, since changing an actorRef would change all the
      * subtree
      */
-   final def insert(newRef: A, at: Path): ATree[A] =
-     at match
-      case Nil => if a == newRef then this else ATree[A](newRef)
-      // note here we loose any subtrees below the insertion.
-      case name :: Nil  => ATree(a, kids + (name -> ATree[A](newRef)))
-      case name :: tail =>
-        // todo: stack problem. Need tail recursive version of this
-        val alt = kids.get(name).map(kid => kid.insert(newRef, tail))
-        // if we don't have a child, but we have to add a grandchild, then what should we put
-        // as the value of the child? (it could happen I guess that some message of the grandchild arrives faster?)
-        // here we ignore everything... (it is better for the message to work its way up the hierarchy)
-        // todo: if that is the case, messages that go up the hierarchy too early should be a signal to the
-        //   actors to try to re-register
-        alt match
-         case None    => this // we could not insert anything so we return this
-         case Some(k) => ATree(a, kids + (name -> k))
+   final def insert(newRef: A, at: Path): DirTree[A] =
+     place(at){ _ => new DirTree(newRef)} // we forget all subtrees of dt
+
+   final def place(at: Path)(f: DirTree[A] => DirTree[A]): DirTree[A] =
+      @tailrec
+      def loop(path: APath[A], result: DirTree[A]): DirTree[A] =
+        path match
+         case Nil                 => result
+         case (name, obj) :: tail => loop(tail, DirTree(obj.a, obj.kids + (name -> result)))
+
+       // note here we loose any subtrees below the insertion.
+      this.toClosestAPath(at) match
+          case (Nil, Nil) => f(this)
+          case (Nil, apath) => loop(apath, f(apath.head._2))
+          // something remains, so we can't insert. We stay where we are
+          case other => this
+   end place
+   
 
    /** we need to replace the A at at path, but keep everything else the same. This happens when a
      * property of the ref changes, but not the tree structure, eg. if we keep the same ActorRef but
      * change the info about acls
      */
-   final def replace(newRef: A, at: Path): ATree[A] =
-     at match
-      case Nil => if a == newRef then this else ATree[A](newRef, kids)
-      // note here we loose any subtrees below the insertion.
-      case name :: Nil =>
-        kids.get(name) match
-         case None       => ATree(a, kids + (name -> ATree[A](newRef)))
-         case Some(tree) => ATree(a, kids + (name -> ATree[A](newRef, tree.kids)))
-      case name :: tail =>
-        // todo: stack problem. Need tail recursive version of this
-        val alt = kids.get(name).map(kid => kid.insert(newRef, tail))
-        alt match
-         case None    => this // we could not insert anything so we return this
-         case Some(k) => ATree(a, kids + (name -> k))
+   final def replace(newRef: A, at: Path): DirTree[A] =
+     place(at){ (dt: DirTree[A]) => new DirTree(newRef, dt.kids)}
 
-   final def delete(at: Path): Option[ATree[A]] =
-     at match
-      case Nil => None
-      case name :: tail => // todo: stack problem
-        Some(deleteNE(name, tail))
+   def delete(at: Path): Option[DirTree[A]] =
+     @tailrec
+     def loop(remaining: APath[A], result: DirTree[A]): Option[DirTree[A]] =
+       remaining match
+         case Nil => Some(result)
+         case head::tail =>
+           val (name, dt) = head
+           loop(tail, DirTree(dt.a, dt.kids + (name -> result)))
 
-   final def deleteNE(name: String, remaining: List[String]): ATree[A] =
-     kids.get(name) match
-      case None => this
-      case Some(tree) =>
-        remaining match
-         case Nil => ATree(a, kids - name)
-         case head :: tail => // todo: stack problem
-           val alt = tree.deleteNE(head, tail)
-           ATree(a, kids + (name -> alt))
+     this.toClosestAPath(at) match
+       case (Nil, Nil) => None
+       case (Nil, (name,dt)::apath) => loop(apath,DirTree(dt.a,dt.kids - name))
+     // something remains, so we can't insert. We stay where we are
+       case other => None
+     
+end DirTree
 
-   /** @param at
-     *   path to resource A
-     * @return
-     *   a pair of the remaining path and A
-     */
-   final def findClosest(at: Path): (Path, A) =
-     at match
-      case Nil => (at, a)
-      case name :: tail => // todo: stack problem
-        kids.get(name) match
-         case None => (at, a)
-         case Some(tree) =>
-           tree.findClosest(tail)
-
-end ATree
--- src/test/scala/run/cosy/ldp/TestRegistry.scala
+++ src/test/scala/run/cosy/ldp/TestRegistry.scala
@@ -6,11 +6,13 @@
 
 package run.cosy.ldp
 
+import akka.http.scaladsl.model.Uri
 import munit.*
 import munit.Location.*
-import run.cosy.ldp.ResourceRegistry.*
-import akka.http.scaladsl.model.Uri
 import run.cosy.Solid.pathToList
+import run.cosy.ldp.ResourceRegistry.*
+
+import scala.collection.immutable.HashMap
 
 class PathDBInt extends PathDB[Int]
 
@@ -23,22 +25,30 @@ class TestRegistry extends munit.FunSuite:
 
    // low level tests
    test("On ATree[Int]") {
-     val rt1 = ATree(1)
+     val rt1 = DirTree(1)
 
      val rt1_ = rt1.insert(1, Nil)
-     assert(rt1 eq rt1_)
+     assertEquals(rt1, rt1_)
 
+     val rt1bis = rt1.insert(11, Nil)
+     assertEquals(rt1bis, DirTree(11))
+
+     // the insertion is ignored
      val rt2 = rt1.insert(2, srcTst)
+     assertEquals(rt2, rt1)
+
      assertEquals(rt2.findClosest(Nil), (Nil, 1))
      assertEquals(rt2.findClosest(src), (src, 1))
 
      val rt3 = rt1.insert(2, src)
+     assertEquals(rt3, DirTree(1, HashMap("src" -> DirTree(2))))
      assertEquals(rt3.findClosest(src), (Nil, 2))
 
      val rt4 = rt3.insert(3, srcTst)
      assertEquals(rt4.findClosest(srcTst), (Nil, 3), rt4)
      assertEquals(rt4.findClosest(src), (Nil, 2), rt4)
      assertEquals(rt4.findClosest(srcTstScHello), (srcTstScHello.drop(2), 3), rt4)
+     assertEquals(rt4, DirTree(1, HashMap("src" -> DirTree(2, HashMap("test" -> DirTree(3))))))
 
      val rt5 = rt4.insert(4, srcTsSc)
      assertEquals(rt5.findClosest(src), (Nil, 2), rt5)
@@ -99,4 +109,35 @@ class TestRegistry extends munit.FunSuite:
      assertEquals(dbInt.getActorRef(p2), None, dbInt.pathMap.get())
      assertEquals(dbInt.getActorRef(p1), None, dbInt.pathMap.get())
 
+   }
+
+   test("stack safety") {
+     val root = DirTree(0)
+     val values: Seq[Int] = 1 to 5000
+     val path: Seq[String] = values.map(_.toString)
+     val dts: List[DirTree[Int]] =
+       values.toList.foldLeft(List(root)) { (dts, i) =>
+         dts.head.insert(i, path.take(i).toList)::dts
+       }.reverse
+     //test a few values in dts
+     val dt1 = dts(1)
+     assertEquals(dt1,DirTree(0, HashMap("1" -> DirTree(1))))
+     val dt2 = dts(2)
+     assertEquals(dt2, DirTree(0, HashMap("1" -> DirTree(1, HashMap("2" -> DirTree(2))))))
+     assertEquals(dts(10).findClosest(path.take(9).toList), (Nil, 9))
+     assertEquals(dts(10).findClosest(path.take(11).toList), (List("11"), 10))
+     assertEquals(dts(10).findClosest(path.take(12).toList), (List("11","12"), 10))
+     val dt2000 = dts(2000)
+     assertEquals(dt2000.findClosest(path.take(1999).toList), (Nil, 1999))
+     assertEquals(dt2000.findClosest(path.take(1000).toList), (Nil, 1000))
+     val dt4990 = dts(4990)
+     assertEquals(dt4990.findClosest(path.take(1999).toList), (Nil, 1999))
+     assertEquals(dt4990.findClosest(path.take(1000).toList), (Nil, 1000))
+     assertEquals(dt4990.findClosest(path.take(4990).toList), (Nil, 4990))
+     assertEquals(dt4990.findClosest(path.take(4991).toList), (List("4991"), 4990))
+     assertEquals(dt4990.findClosest(path.take(4992).toList), (List("4991","4992"), 4990))
+     val dt4999 = dts(4999)
+     val dt4991Deleted = dt4999.delete(path.take(4991).toList)
+     assert(dt4991Deleted.isDefined)
+     assertEquals(dt4991Deleted.get,dt4990)
    }
